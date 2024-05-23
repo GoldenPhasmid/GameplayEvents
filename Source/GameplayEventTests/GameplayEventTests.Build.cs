0bd2cf28728d1308bba53c55ca82663631b2522c
Oleksandr <okushnirenko.work@gmail.com>
2024-05-23 22:24:45 +0300

    Move GameplayEvents functionality to separate plugin

---
+++ Source/GameplayEventTests/GameplayEventTests.Build.cs
@@ -0,0 +1,25 @@
+﻿using UnrealBuildTool;
+
+public class GameplayEventTests : ModuleRules
+{
+    public GameplayEventTests(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "GameplayEvents",
+            }
+        );
+    }
+}
