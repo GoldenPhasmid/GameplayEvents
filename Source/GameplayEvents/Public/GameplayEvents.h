c801fab6a24fa79ebe629a525cc3a680de27b2df
Oleksandr <okushnirenko.work@gmail.com>
2024-09-15 00:40:26 +0300

    Add generic gameplay events

--- Source/GameplayEvents/Public/GameplayEvents.h
+++ Source/GameplayEvents/Public/GameplayEvents.h
@@ -2,4 +2,39 @@
 
 #include "CoreMinimal.h"
 
+#include "GameplayEvents.generated.h"
+
 DECLARE_LOG_CATEGORY_EXTERN(LogGameplayEvents, Log, All);
+
+/**
+ * Empty gameplay event
+ */
+USTRUCT(BlueprintType)
+struct FEmptyEvent
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Generic object gameplay event
+ */
+USTRUCT(BlueprintType)
+struct GAMEPLAYEVENTS_API FObjectGameplayEvent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	UObject* Object = nullptr;
+};
+
+/**
+ * Generic actor gameplay event
+ */
+USTRUCT(BlueprintType)
+struct GAMEPLAYEVENTS_API FActorGameplayEvent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	AActor* Actor = nullptr;
+};
